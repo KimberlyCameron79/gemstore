9ed8e0ea6e7359f22869082d44d1a75b29c1e622
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-08-10 16:06:26 -0400

    Conditional Operators

---
+++ OneDrive/Desktop/PracticeJS/app.js
@@ -0,0 +1,189 @@
+
+/*
+// Good!
+let age = 8;
+
+
+//Strings
+const name = "Kim: 'To be or not to be' "
+const name2 = 'Kim'
+const name3 = `Kim`
+
+//Boolean
+const employed = true;
+
+//Undefined
+const example = undefined;
+
+//Null
+const example2 = null;
+
+//String Operator
+let firstName = "Bob"
+let LastName = "The Builder"
+
+//concatenation
+let FullName = firstName + LastName
+let concatenation = "con" + "cat" + "ena" + "nation"
+
+//And 
+
+
+console.log(concatenation);
+
+let a= "Hello World"
+let b= "Today is a great day!"
+c= a+ b 
+print (c)
+// 
+
+findaverage(green;purple;blue;){average +=nums[1]}
+let green = 10;
+let purple =18;
+let blue =7;
+
+let result= green + purple + blue
+
+
+
+let num1=25;
+let num2=10;
+let num3=45;
+let num4=30;
+let num5=13;
+let num6=22;
+let num7=7;
+let num8=31;
+let num9=60;
+let num10=14;
+let result= Num1; num2;num3;num4;num5;num6;num7;num8;num9;num10;
+
+
+
+return average / nums length
+
+find average (4, 10, 22, -30, 55)
+
+find average (68, 7, -22, 9, 100)
+
+
+let box1 = 'hello'
+let box2= 'class'
+let box3
+
+box3 = box1
+box1 = box2
+box2 = box3
+
+console.log('box2=class' +'box1' +box1)
+
+
+let box1 = 10;
+let box2 = 3;
+let temp;
+temp = box1;
+console.log("temp is now " + temp);
+box1 = box2
+console.log("box1 is now " + box1);
+box2 = temp;
+console.log("box2 is now " + box2);
+// 
+*/
+/*
+let boy=25;
+
+if(boy >=24) else 
+{
+   console.log("Can ride the blue scooters")
+} 
+let girl= 26;
+if(girl >=25) else 
+{
+    console.log ("Can ride the pink scooters")
+}
+*/
+/*
+if (time<10){
+    greeting = "Good morning";
+    }
+else if (time < 20){
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+
+if (minutes=<100){
+    Proficient = "Great Shape";
+    }
+ else if(minutes=>100){
+    Intermediate = "Average";
+ } else if (minutes= -100)
+ {
+    Below Average = "Below Average"
+ }
+*/
+let global;
+let blockScoped;
+let nestedBlockScope;
+
+// outter if statement
+if (true) {
+    let nestedBlockScope= "nest"
+console.log(nestedBlockScope)
+console.log(blockScoped)
+}
+else{
+
+}
+
+console.log(global)
+
+// }
+ /*
+ let number = 2;
+ 
+ switch (number){
+case 1:
+case 2:
+case 3:
+    if(number === 1)
+        {
+            console.log ('the number is 1')               
+ } else{
+    console.log('inside else')
+ }
+ console.log('inside case 3')
+ case 4:
+    console.log('this too')
+        default:
+            console.log('do something else')
+           
+ }
+*/
+/*
+//ternary operator (three parts)
+
+Let lighting = 'dark'
+
+lighting === 'dark'? 'night time': 'day time'// 
+*/
+/*
+Let letter = A
+
+switch (letter) {
+    Case "A":
+    Case "B":
+    Case "C":
+    Case "D":
+    if(letter===vowel)
+    {
+        //console.log('the letter is a vowel')
+    } else {
+        //console.log('the letter is a constant')
+    } break;
+    default: 
+        //console.log ('Do something else')
+    break;
+}*/
+
+let number = 2;

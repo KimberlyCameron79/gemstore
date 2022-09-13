6d9cdcce42a5642e375de2330d255c0511e2b346
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-09-13 16:09:18 -0400

    game

--- OneDrive/Desktop/PerScholas/Project/app.js
+++ OneDrive/Desktop/PerScholas/Project/app.js
@@ -1,4 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const squares =document.querySelectorAll('.grid div')
+const playerOne ="G";
+const playerTwo ="P";
+const playingNow = playerOne;
 
-})
+const gameOver = false;
+let grid;
+
+let rows = 6;
+let columns = 7;
+let curColumns = [];   //keeps track of which column theyre currently at.
+
+window.onload = function() {
+    startGame();
+}
+
+function startGame (){
+    grid = [];
+    curColumns = [5, 5, 5, 5, 5, 5];
+
+    for (let r =0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(' ');
+
+            let grid= document.createElement("div");
+            grid.id =r.toString() + "-" + c.toString();
+            grid.classList.add("tile");
+            grid.addEventListener("click", setChip);
+        }
+        grid.push(row);
+    }
+}
